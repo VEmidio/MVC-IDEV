a16cdf77e32b0bcfba6285b66c10c055f5d261dc
Vinicius Emidio <vinicius.s.emidio@gmail.com>
2020-08-27 22:59:11 -0300

    add get end set

--- Models/User.js
+++ Models/User.js
@@ -6,7 +6,41 @@ class Users {
         this._password = password;
         this._nome = nome;
     }
+
+    get id() {
+        return this._id;
+    }
+
+    get email() {
+        return this._email
+    }
+
+    get password() {
+        return this._password
+    }
+
+    get nome() {
+        return this._nome
+    }
+
+    set id(id) {
+        this._id = id
+    }
+
+    set email(email) {
+        this._email = email
+    }
+    set password(password) {
+        this._password = password;
+    }
+    set nome(nome) {
+        this._nome = nome;
+    }
+
+
+
 }
+
 
 
 module.exports = Users;
