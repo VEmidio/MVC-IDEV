e5472ff45aa7a6a3a8991c866aacb53b98726b69
Diogo Costa de Paula <costadiogo30@gmail.com>
2020-08-27 22:50:19 -0300

    Adicionado o arquivo database.js e a classe users.

---
+++ Models/User.js
@@ -0,0 +1,12 @@
+class Users {
+
+    constructor() {
+        this._id = id;
+        this._email = email;
+        this._password = password;
+        this._nome = nome;
+    }
+}
+
+
+module.exports = Users;
